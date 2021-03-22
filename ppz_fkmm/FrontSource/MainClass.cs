6e12f378b322ecc8ed089865f7239ecfe195f9be
Kacu <k.ptychu@gmail.com>
2021-03-22 14:05:03 +0100

    init project

---
+++ ppz_fkmm/FrontSource/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ppz_fkmm
+{
+    static class MainClass
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
---
+++ ppz_fkmm/FrontSource/MainWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ppz_fkmm.BackSource;
+
+
+namespace ppz_fkmm
+{
+    public partial class MainWindow : Form
+    {
+        Program _backend;
+        private System.ComponentModel.IContainer components = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // _____________________________________________________________________________________________________
+        // #####################################################################################################
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "MainWindow";
+            this.ResumeLayout(false);
+
+        }
+        // _____________________________________________________________________________________________________
+        // #####################################################################################################
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
