718b35ac68fac6c56d9f23833cd83b8f00c93cf9
Kacu <kacper.kaleta@scythe-studio.com>
2021-06-18 11:01:55 +0200

    completed changes

--- ppz_fkmm/FrontSource/Pages/SettingsPanel.Designer.cs
+++ ppz_fkmm/FrontSource/Pages/SettingsPanel.Designer.cs
@@ -37,90 +37,149 @@ namespace ppz_fkmm.FrontSource.Pages
             this.labelOldPass = new System.Windows.Forms.Label();
             this.labelNewAdress = new System.Windows.Forms.Label();
             this.labelNewPass = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // settingsBtnPass
             // 
-            this.settingsBtnPass.Location = new System.Drawing.Point(114, 86);
+            this.settingsBtnPass.AutoSize = true;
+            this.settingsBtnPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.settingsBtnPass.Location = new System.Drawing.Point(4, 136);
+            this.settingsBtnPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.settingsBtnPass.Name = "settingsBtnPass";
-            this.settingsBtnPass.Size = new System.Drawing.Size(100, 23);
+            this.settingsBtnPass.Size = new System.Drawing.Size(200, 36);
             this.settingsBtnPass.TabIndex = 0;
-            this.settingsBtnPass.Text = "Zmień Hasło";
+            this.settingsBtnPass.Text = "Change Password";
             this.settingsBtnPass.UseVisualStyleBackColor = true;
             // 
             // settingsBtnAdress
             // 
-            this.settingsBtnAdress.Location = new System.Drawing.Point(114, 165);
+            this.settingsBtnAdress.AutoSize = true;
+            this.settingsBtnAdress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.settingsBtnAdress.Location = new System.Drawing.Point(4, 272);
+            this.settingsBtnAdress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.settingsBtnAdress.Name = "settingsBtnAdress";
-            this.settingsBtnAdress.Size = new System.Drawing.Size(100, 23);
+            this.settingsBtnAdress.Size = new System.Drawing.Size(184, 36);
             this.settingsBtnAdress.TabIndex = 1;
-            this.settingsBtnAdress.Text = "Zmień Adres";
+            this.settingsBtnAdress.Text = "Change Address";
             this.settingsBtnAdress.UseVisualStyleBackColor = true;
             // 
             // settingsOldPass
             // 
-            this.settingsOldPass.Location = new System.Drawing.Point(114, 33);
+            this.settingsOldPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.settingsOldPass.Location = new System.Drawing.Point(4, 30);
+            this.settingsOldPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.settingsOldPass.Name = "settingsOldPass";
-            this.settingsOldPass.Size = new System.Drawing.Size(100, 20);
+            this.settingsOldPass.Size = new System.Drawing.Size(556, 32);
             this.settingsOldPass.TabIndex = 2;
             // 
             // settingsNewPass
             // 
-            this.settingsNewPass.Location = new System.Drawing.Point(114, 59);
+            this.settingsNewPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.settingsNewPass.Location = new System.Drawing.Point(4, 96);
+            this.settingsNewPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.settingsNewPass.Name = "settingsNewPass";
-            this.settingsNewPass.Size = new System.Drawing.Size(100, 20);
+            this.settingsNewPass.Size = new System.Drawing.Size(556, 32);
             this.settingsNewPass.TabIndex = 4;
             // 
             // settingsAdress
             // 
-            this.settingsAdress.Location = new System.Drawing.Point(114, 127);
+            this.settingsAdress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.settingsAdress.Location = new System.Drawing.Point(4, 232);
+            this.settingsAdress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.settingsAdress.Name = "settingsAdress";
-            this.settingsAdress.Size = new System.Drawing.Size(100, 20);
+            this.settingsAdress.Size = new System.Drawing.Size(556, 32);
             this.settingsAdress.TabIndex = 6;
             // 
             // labelOldPass
             // 
             this.labelOldPass.AutoSize = true;
-            this.labelOldPass.Location = new System.Drawing.Point(43, 36);
+            this.labelOldPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.labelOldPass.ForeColor = System.Drawing.Color.Snow;
+            this.labelOldPass.Location = new System.Drawing.Point(4, 0);
+            this.labelOldPass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelOldPass.Name = "labelOldPass";
-            this.labelOldPass.Size = new System.Drawing.Size(65, 13);
+            this.labelOldPass.Size = new System.Drawing.Size(154, 26);
             this.labelOldPass.TabIndex = 7;
-            this.labelOldPass.Text = "Stare hasło:";
+            this.labelOldPass.Text = "Old Password:";
             // 
             // labelNewAdress
             // 
             this.labelNewAdress.AutoSize = true;
-            this.labelNewAdress.Location = new System.Drawing.Point(43, 130);
+            this.labelNewAdress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.labelNewAdress.ForeColor = System.Drawing.Color.Snow;
+            this.labelNewAdress.Location = new System.Drawing.Point(4, 202);
+            this.labelNewAdress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelNewAdress.Name = "labelNewAdress";
-            this.labelNewAdress.Size = new System.Drawing.Size(67, 13);
+            this.labelNewAdress.Size = new System.Drawing.Size(148, 26);
             this.labelNewAdress.TabIndex = 8;
-            this.labelNewAdress.Text = "Nowy Adres:";
+            this.labelNewAdress.Text = "New Address:";
             // 
             // labelNewPass
             // 
             this.labelNewPass.AutoSize = true;
-            this.labelNewPass.Location = new System.Drawing.Point(43, 62);
+            this.labelNewPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.labelNewPass.ForeColor = System.Drawing.Color.Snow;
+            this.labelNewPass.Location = new System.Drawing.Point(4, 66);
+            this.labelNewPass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelNewPass.Name = "labelNewPass";
-            this.labelNewPass.Size = new System.Drawing.Size(68, 13);
+            this.labelNewPass.Size = new System.Drawing.Size(164, 26);
             this.labelNewPass.TabIndex = 9;
-            this.labelNewPass.Text = "Nowe hasło:";
+            this.labelNewPass.Text = "New Password:";
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.labelOldPass);
+            this.flowLayoutPanel3.Controls.Add(this.settingsOldPass);
+            this.flowLayoutPanel3.Controls.Add(this.labelNewPass);
+            this.flowLayoutPanel3.Controls.Add(this.settingsNewPass);
+            this.flowLayoutPanel3.Controls.Add(this.settingsBtnPass);
+            this.flowLayoutPanel3.Controls.Add(this.label1);
+            this.flowLayoutPanel3.Controls.Add(this.labelNewAdress);
+            this.flowLayoutPanel3.Controls.Add(this.settingsAdress);
+            this.flowLayoutPanel3.Controls.Add(this.settingsBtnAdress);
+            this.flowLayoutPanel3.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(561, 354);
+            this.flowLayoutPanel3.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panel1.Controls.Add(this.flowLayoutPanel3);
+            this.panel1.Location = new System.Drawing.Point(5, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(567, 362);
+            this.panel1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(4, 176);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 26);
+            this.label1.TabIndex = 10;
             // 
             // SettingsPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.labelNewPass);
-            this.Controls.Add(this.labelNewAdress);
-            this.Controls.Add(this.labelOldPass);
-            this.Controls.Add(this.settingsAdress);
-            this.Controls.Add(this.settingsNewPass);
-            this.Controls.Add(this.settingsOldPass);
-            this.Controls.Add(this.settingsBtnAdress);
-            this.Controls.Add(this.settingsBtnPass);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SettingsPanel";
-            this.Size = new System.Drawing.Size(340, 273);
+            this.Size = new System.Drawing.Size(576, 370);
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -134,5 +193,8 @@ namespace ppz_fkmm.FrontSource.Pages
         private System.Windows.Forms.Label labelOldPass;
         private System.Windows.Forms.Label labelNewAdress;
         private System.Windows.Forms.Label labelNewPass;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
